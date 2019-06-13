ba9c5c540ae34a11722191d643d32737ff0c4410
INUPURNOMO <social.inupurnomo@gmail.com>
2019-06-13 08:54:55 +0700

    update db

---
+++ DATABASE/dash_keu.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 13, 2019 at 03:01 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dash_keu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `jenis_kelamin` char(1) NOT NULL,
+  `no_telp` varchar(15) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `password` varchar(9999) NOT NULL,
+  `bio` varchar(9999) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `nama`, `email`, `jenis_kelamin`, `no_telp`, `alamat`, `password`, `bio`) VALUES
+(101, 'admin', 'Ilham Ibnu Purnomo', 'inupurnomo@outlook.com', 'L', '085723688655', 'Dayeuh Kolot', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum recusandae ut aperiam velit quidem facere voluptatibus corrupti earum nihil consequatur, totam, dicta illo minus similique.'),
+(102, 'adis', 'Adisca Naufal R', 'adisca@gmail.com', 'L', '085852586546', 'Bekasi', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', ''),
+(103, 'supplier', 'Ilham Ibnu Purnomo', 'social.inupurnomo@gmail.com', '', '', '', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', ''),
+(104, 'karyawanPur', 'Adisca Naufal R', 'iptutorplus@gmail.com', '', '', '', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', ''),
+(105, 'phusink', 'Ilham Ibnu Purnomo', 'inuofficial@gmail.com', '', '', '', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', ''),
+(106, 'test', 'Adisca Naufal R', 'inuofficial@gmail.com', '', '', '', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
