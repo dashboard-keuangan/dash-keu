555e3c0067e8e4bc81985d867955a2cb7749def9
Ahmad Rizky Prayogi <a.rizkyprayogi@gmail.com>
2019-07-23 03:59:20 +0700

    .

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Pages extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
+		date_default_timezone_set('Asia/Jakarta');
 		$this->load->helper('date');
 		$this->load->model('m_user', '', TRUE);
 		$this->load->model('m_keuangan', '', TRUE);
